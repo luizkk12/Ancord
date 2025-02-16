e2cdd46900937133f554f3dc2b15ca16702bb713
LuizKK12 <115504242+luizkk12@users.noreply.github.com>
2025-02-16 15:31:57 -0300

    Add files via upload

--- src/commands/economy/bal.js
+++ src/commands/economy/bal.js
@@ -1,1 +1,39 @@
+const Discord = require('discord.js');
+const Database = require('quick.db');
+const db = new Database.QuickDB();
 
+module.exports = {
+  data: {
+    name: 'bal',
+    description: 'Veja o saldo de algum usuário.',
+    usage: 'bal [usuário?]',
+    category: 'Economia'
+  },
+  
+  execute: async (message, args) => {
+    let user;
+    let regex = /<@[!]?(\d+)>/;
+    let getUser = String(args[0]).match(regex);
+    
+    if (getUser) {
+      user = await message.client.users.fetch(getUser[1]).catch(() => null);
+    } else {
+      user = await message.client.users.fetch(args[0]).catch(() => null);
+    }
+    
+    if (!user) user = message.author;
+    let saldo = await db.get(`diamonds_${user.id}`) || 0;
+    
+    let embedDescription = user.id === message.author.id ? `**💎 Atualmente, você possui ${saldo} diamantes.**` : `**💎 Atualmente, ${user.username} possui ${saldo} diamantes.**`;
+    
+    let embed = new Discord.EmbedBuilder()
+    .setTitle(`**💰 | Saldo de ${user.username}**`)
+    .setDescription(embedDescription)
+    .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+    .setFooter({ text: `Comando executado por ${message.author.username}` })
+    .setColor(0x00ff00)
+    .setTimestamp();
+    
+    await message.reply({ embeds: [embed] });
+  }
+}
