b14747e303d5ded08c8f366499e8e7df2ff19c5e
LuizKK12 <115504242+luizkk12@users.noreply.github.com>
2025-02-25 19:36:48 -0300

    Add files via upload

--- src/config/events/messageCreate.js
+++ src/config/events/messageCreate.js
@@ -6,7 +6,10 @@ module.exports = {
   name: Discord.Events.MessageCreate,
   execute: async message => {
     let prefix = await db.get(`prefix_${message.guild.id}`) || '.';
-    
+    let botMember = message.channel.permissionsFor(message.client.user.id);
+
+    if (!botMember.has(Discord.PermissionsBitField.Flags.SendMessages) || !botMember.has(Discord.PermissionsBitField.Flags.ViewChannel)) return;
+
     if (message.author.bot) return;
     if (message.content === `<@${message.client.user.id}>` || message.content === `<@!${message.client.user.id}>`) {
       let embed = new Discord.EmbedBuilder()
@@ -19,6 +22,20 @@ module.exports = {
       return await message.reply({ embeds: [embed] });
     }
     if (!message.content.startsWith(prefix)) return;
+
+    let blacklist = await db.get(`blacklist_${message.author.id}`) || 'não';
+    let motivobl = await db.get(`motivobl_${message.author.id}`) || 'Indefinido';
+
+    if (blacklist === 'sim') {
+      let embed = new Discord.EmbedBuilder()
+      .setTitle(`**🚫 | Você foi banido.**`)
+      .setDescription(`**❌ Eu não executarei mais comandos pra você porque você está proibido de executar meus comandos. Meu criador deixou um comentário:**\n\n__${motivobl}__`)
+      .setFooter({ text: `Comando executado por ${message.author.username}` })
+      .setColor(0xff0000)
+      .setTimestamp();
+
+      return await message.reply({ embeds: [embed] });
+    }
     
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const commandName = args.shift().toLowerCase();
